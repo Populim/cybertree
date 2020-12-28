77df2607b35435701ba490ceaa6a6b0fa35d631a
MatheusPopulim3 <44553903+Populim@users.noreply.github.com>
2020-12-28 15:52:05 -0300

    semifinal

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var perks = all_perks[0];
-var current_skill = 0;
+var cs = 0;
 var bool_skills = [0,0,0,0,0,0,0,0,0,0,0,0];
 var skill_size = [0,0,0,0,0,0,0,0,0,0,0,0];
 
@@ -26,8 +26,8 @@ function calcSkillSize(){
 //transform current perks to binary string
 function to_binary(){
 	var nDig = 0
-	for (var i = 0; i < perks.length; i++) {
-		if(perks[i].mV < 2){
+	for (var i = 0; i < all_perks[cs].length; i++) {
+		if(all_perks[cs][i].mV < 2){
 			nDig++;
 		}
 		else{
@@ -35,11 +35,11 @@ function to_binary(){
 		}
 	}
 	var binHash = ""
-	for (var i = 0; i < perks.length; i++) {
-		if(perks[i].mV > 1 && perks[i].aV < 2){
+	for (var i = 0; i < all_perks[cs].length; i++) {
+		if(all_perks[cs][i].mV > 1 && all_perks[cs][i].aV < 2){
 			binHash += "0";
 		}
-		binHash += (perks[i].aV >>> 0).toString(2);
+		binHash += (all_perks[cs][i].aV >>> 0).toString(2);
 	}
 	return binHash;
 }
@@ -47,16 +47,16 @@ function to_binary(){
 function fromBinary(hsh){
 	var index = 0;
 	var size = 1;
-	for (var i = 0; i < perks.length; i++) {
+	for (var i = 0; i < all_perks[cs].length; i++) {
 		size = 1;
-		if(perks[i].mV > 1){
+		if(all_perks[cs][i].mV > 1){
 			size = 2;
 		}
 		if(index + size > hsh.length){
-			perks[i].aV = 0;
+			all_perks[cs][i].aV = 0;
 		}
 		else{
-			perks[i].aV = parseInt(hsh.slice(index, index+size),2);
+			all_perks[cs][i].aV = parseInt(hsh.slice(index, index+size),2);
 		}
 		index = index + size;
 	}
@@ -120,11 +120,11 @@ function getURLHash(){
 function setClickAction(){
 	var bt;
 	bt = document.querySelectorAll(".myDiv1");
-	for (var i = 0; i < perks.length; i++) {
-		if(perks[i].aV == 0){
+	for (var i = 0; i < all_perks[cs].length; i++) {
+		if(all_perks[cs][i].aV == 0){
 			bt[i].style.background = "#ffffff";
 		}
-		else if (perks[i].aV == perks[i].mV){
+		else if (all_perks[cs][i].aV == all_perks[cs][i].mV){
 			bt[i].style.background = "#d4af37";
 		}
 		else{
@@ -140,36 +140,37 @@ function showDescription(){
 	var div_desc;
 
 	div_desc = document.getElementById("description_name");
-	div_desc.innerHTML = "Name: " + perks[value].name;
+	div_desc.innerHTML = "Name: " + all_perks[cs][value].name;
 	
 	div_desc = document.getElementById("description_type");
 	div_desc.innerHTML = "Type: Passive";
 
 	div_desc = document.getElementById("description_effect");
-	div_desc.innerHTML = "Effect: " + perks[value].effect;
+	div_desc.innerHTML = "Effect: " + all_perks[cs][value].effect;
 	
 	div_desc = document.getElementById("description_required");
-	div_desc.innerHTML = "Attribute Level Required: "+"20";
+	div_desc.innerHTML = "Attribute Level Required: "+String(all_perks[cs][value].rL);
 	
 	div_desc = document.getElementById("description_cl_ml");
-	div_desc.innerHTML = "Current level: " + String(perks[value].aV) + "/" + String(perks[value].mV);
+	div_desc.innerHTML = "Current level: " + String(all_perks[cs][value].aV) + "/" + String(all_perks[cs][value].mV);
 }
 
 
 
 function clicounacoisa(){
 	var value = parseInt(this.id);
-	if(perks[value].aV<perks[value].mV){
-		perks[value].aV++;
+	console.log("current skill", cs);
+	if(all_perks[cs][value].aV<all_perks[cs][value].mV){
+		all_perks[cs][value].aV++;
 	}
 	else{
-		perks[value].aV = 0;
+		all_perks[cs][value].aV = 0;
 	}
 
-	if(perks[value].aV == 0){
+	if(all_perks[cs][value].aV == 0){
 		this.style.background = "#ffffff";
 	}
-	else if (perks[value].aV == perks[value].mV){
+	else if (all_perks[cs][value].aV == all_perks[cs][value].mV){
 		this.style.background = "#d4af37";
 	}
 	else{
@@ -177,21 +178,27 @@ function clicounacoisa(){
 	}
 
 	var status = document.getElementById("level"+String(value));
-	status.innerHTML = String(perks[value].aV) + '/'+ String(perks[value].mV);
+	status.innerHTML = String(all_perks[cs][value].aV) + '/'+ String(all_perks[cs][value].mV);
 	var status = document.getElementById("description_cl_ml");
-	status.innerHTML = "Current level: " + String(perks[value].aV) + "/" + String(perks[value].mV);
+	status.innerHTML = "Current level: " + String(all_perks[cs][value].aV) + "/" + String(all_perks[cs][value].mV);
 	setURLHash();
 	//window.location.hash = binary_tobase64(to_binary());
 }
 
 function setURLHash(){
 	var hsh = "";
+	var flag1 = 0;
 	for (var i = 0; i < all_perks.length; i++) {
+		flag1 = 0;
 		for (var j = 0; j < all_perks[i].length; j++) {
 			if(all_perks[i][j].aV != 0){
 				bool_skills[i] = 1;
+				flag1 = 1;
 				break;
 			}
+		}
+		if(flag1 == 0){
+			bool_skills[i] = 0;
 		}
 	}
 	var hsh1 = "";
@@ -202,11 +209,20 @@ function setURLHash(){
 	hsh += binary_tobase64(hsh1);
 	console.log(binary_tobase64(hsh1));
 	console.log(hsh1);
-	
+	hsh1 = getURLHash();
+	var aux = 2;
 	for (var i = 0; i < bool_skills.length; i++) {
 		if(bool_skills[i] == 1){
-			perks = all_perks[i];
-			hsh += binary_tobase64(to_binary());
+			
+			if(cs == i){
+				hsh += binary_tobase64(to_binary());
+			}
+			else{
+				hsh += hsh1.slice(aux,aux+skill_size[i]);
+				//hsh += window.location
+			}
+			aux += skill_size[i];
+			//perks = all_perks[i];
 		}
 	}
 
@@ -219,7 +235,7 @@ function processURLHash(){
 	var bool_aux = "";
 	var aux2;
 	var aux3 = "";
-	var auxperks = perks;
+	var auxperks = cs;
 	if(hsh.length < 3){
 		console.log("null url");
 	}
@@ -232,31 +248,38 @@ function processURLHash(){
 			if(bool_skills[i] == 1){
 				aux3 = hsh.slice(aux2,Math.min(aux2+skill_size[i],hsh.length));
 				console.log(aux3);
-				aux2 += skill_size[i]
-				perks = all_perks[i];
+				aux2 += skill_size[i];
+				cs = i;
 				fromBinary(base64_tobinary(aux3));
 				console.log(base64_tobinary(aux3));
 			}
 		}
 	}
-	perks = auxperks;
+	cs = auxperks;
 }
 
 
 function loadWindowSkill(){
 	var id = this.getAttribute('data-index');
-	current_skill = id;
-	perks = all_perks[id];
+
+	var all = document.getElementsByClassName('Skill_button');
+	for (var i = 0; i < all.length; i++) {
+	  all[i].style.border = '2px outset blue';
+	}
+	this.style.border = "4px outset blue";
+	
+	cs = id;
+	//perks = all_perks[id];
 	var x = document.getElementById("mainWindow");
 	
-	processURLHash();
+	//processURLHash();
 	//hash = getURLHash();
 	//fromBinary(base64_tobinary(hash));
 	
 
 	x.innerHTML = "";
 	console.log("teste");
-    for (var i = 0; i < perks.length; i++ ){
+    for (var i = 0; i < all_perks[cs].length; i++ ){
 		//console.log("teste")
 		var position = 14+6*(i%7);
 		var margem = '';
@@ -272,12 +295,12 @@ function loadWindowSkill(){
 
 		//'style = "left:' + String(position) + '%;
 
-    	x.innerHTML += '<div title="' + String(perks[i].effect) +
+    	x.innerHTML += '<div title="' + String(all_perks[cs][i].effect) +
     	'" class = "myDiv1" id = "' + String(i) +'" '+
     	margem +
-    	 '> ' + String(perks[i].name) +
+    	 '> ' + String(all_perks[cs][i].name) +
     	 
-    	 '<p class="skill_level" id = "'+ 'level' + String(i) +'">' + String(perks[i].aV) + '/'+ String(perks[i].mV) + '</p>' + ' </div>\n';
+    	 '<p class="skill_level" id = "'+ 'level' + String(i) +'">' + String(all_perks[cs][i].aV) + '/'+ String(all_perks[cs][i].mV) + '</p>' + ' </div>\n';
     	if(((i+1)%7) == 0){
     		x.innerHTML += '<div class="break"></div>\n';
     	}
@@ -289,6 +312,12 @@ function loadWindowSkill(){
 function loadSkillsSubMenu(){
 	var bt;
 	var id = this.getAttribute('data-index');
+
+	var all = document.getElementsByClassName('Attribute_button');
+	for (var i = 0; i < all.length; i++) {
+	  all[i].style.border = '2px outset blue';
+	}
+	this.style.border = "4px outset blue";
 	console.log(id);
 	var ls_index = [[0,1,2],[3,4,5],[6,7],[8,9],[10,11]];
 	var list_skills = ["Athletics","Annihilation","Street Brawler",
@@ -300,7 +329,7 @@ function loadSkillsSubMenu(){
 	var x =	document.getElementById("skill_selector_div");
 	x.innerHTML = "";
 	for (var i = 0; i < ls_index[id].length; i++) {
-		x.innerHTML += '<div class="Attribute_button" '+
+		x.innerHTML += '<div class="Skill_button" '+
 		'data-index = "'+ls_index[id][i]+'" '+
 		'id="skill_button'+ls_index[id][i]+'"'
 		+'>'+list_skills[ls_index[id][i]]+'</div>\n';
@@ -318,6 +347,7 @@ function loadSkillsSubMenu(){
 
 function loadAttributeMenu(){
 	var bt;
+	processURLHash();
 	var list_buttons = ["ab_body","ab_reflex","ab_technical","ab_cool","ab_intel"];
 	for (var i = 0; i < list_buttons.length; i++) {
 		bt = document.getElementById(list_buttons[i]);
@@ -331,7 +361,7 @@ function loadWindow(){
 	//fromBinary(base64_tobinary(hash));
 	x.innerHTML = "";
 	console.log("teste");
-    for (var i = 0; i < perks.length; i++ ){
+    for (var i = 0; i < all_perks[cs].length; i++ ){
 		//console.log("teste")
 		var position = 14+6*(i%7);
 		var margem = '';
@@ -347,12 +377,12 @@ function loadWindow(){
 
 		//'style = "left:' + String(position) + '%;
 
-    	x.innerHTML += '<div title="' + String(perks[i].effect) +
+    	x.innerHTML += '<div title="' + String(all_perks[cs][i].effect) +
     	'" class = "myDiv1" id = "' + String(i) +'" '+
     	margem +
-    	 '> ' + String(perks[i].name) +
+    	 '> ' + String(all_perks[cs][i].name) +
     	 
-    	 '<p class="skill_level" id = "'+ 'level' + String(i) +'">' + String(perks[i].aV) + '/'+ String(perks[i].mV) + '</p>' + ' </div>\n';
+    	 '<p class="skill_level" id = "'+ 'level' + String(i) +'">' + String(all_perks[cs][i].aV) + '/'+ String(all_perks[cs][i].mV) + '</p>' + ' </div>\n';
     	if(((i+1)%7) == 0){
     		x.innerHTML += '<div class="break"></div>\n';
     	}
