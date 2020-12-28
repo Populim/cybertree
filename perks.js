a2c9ae0bf837f05f5cfabd95b552a8f5c598d623
MatheusPopulim3 <44553903+Populim@users.noreply.github.com>
2020-12-28 09:57:51 -0300

    All functionalities working
    All functionalities working. Just need more visual design features and content of 10 skill perks.

--- perks.js
+++ perks.js
@@ -97,183 +97,220 @@ var athletics = [
 		rL:1
 	},
 	{
-		name: "Epimorphosis",
-		ef: ["Health regenerates up to 70% of max Health outside of combat.",
-		"Health regenerates up to 80% of max Health outside of combat.",
-		"Health regenerates up to 90% of max Health outside of combat."],
+		name: "True Grit",
+		ef: ["Increases max Stamina by 10%.",
+		"Increases max Stamina by 20%.",
+		"Increases max Stamina by 30%."],
 		type:"Passive Body Perk",
 		mV:3,
 		aV:0,
-		rL:7
+		rL:5
 	},
 	{
-		name: "Epimorphosis",
-		ef: ["Health regenerates up to 70% of max Health outside of combat.",
-		"Health regenerates up to 80% of max Health outside of combat.",
-		"Health regenerates up to 90% of max Health outside of combat."],
-		type:"Passive Body Perk",
-		mV:3,
-		aV:0,
-		rL:7
-	},
-	{
-		name: "Epimorphosis",
-		ef: ["Health regenerates up to 70% of max Health outside of combat.",
-		"Health regenerates up to 80% of max Health outside of combat.",
-		"Health regenerates up to 90% of max Health outside of combat."],
-		type:"Passive Body Perk",
-		mV:3,
-		aV:0,
-		rL:7
-	},
-	{
-		name: "Epimorphosis",
-		ef: ["Health regenerates up to 70% of max Health outside of combat.",
-		"Health regenerates up to 80% of max Health outside of combat.",
-		"Health regenerates up to 90% of max Health outside of combat."],
-		type:"Passive Body Perk",
-		mV:3,
-		aV:0,
-		rL:7
-	},
-	{
-		name: "Epimorphosis",
-		ef: ["Health regenerates up to 70% of max Health outside of combat.",
-		"Health regenerates up to 80% of max Health outside of combat.",
-		"Health regenerates up to 90% of max Health outside of combat."],
-		type:"Passive Body Perk",
-		mV:3,
-		aV:0,
-		rL:7
-	},
-	{
-		name: "Epimorphosis",
-		ef: ["Health regenerates up to 70% of max Health outside of combat.",
-		"Health regenerates up to 80% of max Health outside of combat.",
-		"Health regenerates up to 90% of max Health outside of combat."],
-		type:"Passive Body Perk",
-		mV:3,
-		aV:0,
-		rL:7
-	},
-	{
-		name: "Epimorphosis",
-		ef: ["Health regenerates up to 70% of max Health outside of combat.",
-		"Health regenerates up to 80% of max Health outside of combat.",
-		"Health regenerates up to 90% of max Health outside of combat."],
-		type:"Passive Body Perk",
-		mV:3,
-		aV:0,
-		rL:7
-	},
-	{
-		name: "Epimorphosis",
-		ef: ["Health regenerates up to 70% of max Health outside of combat.",
-		"Health regenerates up to 80% of max Health outside of combat.",
-		"Health regenerates up to 90% of max Health outside of combat."],
-		type:"Passive Body Perk",
-		mV:3,
-		aV:0,
-		rL:7
-	},
-	{
-		name: "Epimorphosis",
-		ef: ["Health regenerates up to 70% of max Health outside of combat.",
-		"Health regenerates up to 80% of max Health outside of combat.",
-		"Health regenerates up to 90% of max Health outside of combat."],
-		type:"Passive Body Perk",
-		mV:3,
-		aV:0,
-		rL:7
-	},
-	{
-		name: "Epimorphosis",
-		ef: ["Health regenerates up to 70% of max Health outside of combat.",
-		"Health regenerates up to 80% of max Health outside of combat.",
-		"Health regenerates up to 90% of max Health outside of combat."],
-		type:"Passive Body Perk",
-		mV:3,
-		aV:0,
-		rL:7
-	},
-	{
-		name: "Epimorphosis",
-		ef: ["Health regenerates up to 70% of max Health outside of combat.",
-		"Health regenerates up to 80% of max Health outside of combat.",
-		"Health regenerates up to 90% of max Health outside of combat."],
-		type:"Passive Body Perk",
-		mV:3,
-		aV:0,
-		rL:7
-	}
-]
-
-var annihilation = [
-	{
-		name: "The Rock:",
-		ef: ["Enemies cannot knock you down."],
-		type:"Passive Body Perk",
-		mV:1,
-		aV:0,
-		rL:20
-	},//0
-	{
-		name: "Cardio Cure",
-		ef: ["Health Regenerates 25% faster as you move."],
-		type:"Passive Body Perk",
-		mV:1,
-		aV:0,
-		rL:12
-	},
-	{
-		name: "Gladiator",
-		ef: ["Reduces the amount of Stamina consumed when blocking melee attacks by 20%.",
-		"Reduces the amount of Stamina consumed when blocking melee attacks by 40%."],
-		type:"Passive Body Perk",
-		mV:2,
-		aV:0,
-		rL:9
-	},
-	{
-		name: "Pack Mule",
-		ef: ["Increases carrying capacity by 60."],
-		type:"Passive Body Perk",
-		mV:1,
-		aV:0,
-		rL:1
-	},
-	{
-		name: "Epimorphosis",
-		ef: ["Health regenerates up to 70% of max Health outside of combat.",
-		"Health regenerates up to 80% of max Health outside of combat.",
-		"Health regenerates up to 90% of max Health outside of combat."],
-		type:"Passive Body Perk",
-		mV:3,
-		aV:0,
-		rL:7
-	},//4
-	{
-		name: "Like a Butterfly",
-		ef: ["Dodging does not drain Stamina."],
+		name: "Multitasker",
+		ef: ["Allows you to shoot while sprinting, sliding and vaulting."],
 		type:"Passive Body Perk",
 		mV:1,
 		aV:0,
 		rL:11
 	},
 	{
-		name: "Dog of War",
-		ef: ["Increases Health Regen in combat by 15%.",
-		"Increases Health Regen in combat by 20%."],
+		name: "Marathoner",
+		ef: ["Sprinting does not drain Stamina."],
+		type:"Passive Body Perk",
+		mV:1,
+		aV:0,
+		rL:14
+	},
+	{
+		name: "Indestructible",
+		ef: ["Reduces all incoming damage by 10%."],
+		type:"Passive Body Perk",
+		mV:1,
+		aV:0,
+		rL:20
+	},
+	{
+		name: "Human Shield",
+		ef: ["Increases Armor by 20% when grappling an enemy"],
+		type:"Passive Body Perk",
+		mV:1,
+		aV:0,
+		rL:14
+	},
+	{
+		name: "Divided Attention",
+		ef: ["Allows you to reload while sprinting, sliding and vaulting"],
+		type:"Passive Body Perk",
+		mV:1,
+		aV:0,
+		rL:11
+	},
+	{
+		name: "Invincible",
+		ef: ["Increases max Health by 10%.",
+		"Increases max Health by 20%.",
+		"Increases max Health by 30%."],
+		mV:3,
+		aV:0,
+		rL:5
+	},
+	{
+		name: "Soft on your feet",
+		ef: ["Reduces fall damage by 5%.",
+		"Reduces fall damage by 10%.",
+		"Reduces fall damage by 15%.",],
+		type:"Passive Body Perk",
+		mV:3,
+		aV:0,
+		rL:7
+	},
+	{
+		name: "Transporter",
+		ef: ["Allows you to shoot with Pistols and Revolvers or sprint while carrying a body."],
+		type:"Passive Body Perk",
+		mV:1,
+		aV:0,
+		rL:12
+	},
+	{
+		name: "Wolverine",
+		ef: ["Health regen activates 50% faster during combat.",
+		"Health regen activates 90% faster during combat."],
 		type:"Passive Body Perk",
 		mV:2,
 		aV:0,
 		rL:16
 	},
 	{
+		name: "Hard Motherfucker",
+		ef: ["When entering combat, Armor and Resistances increase by 10% for 10sec.<br>"+
+		"+1% per Perk level." ],
+		type:"Passive Body Perk",
+		mV:1,
+		aV:0,
+		rL:20
+	}
+]
+
+var annihilation = [
+	{
+		name: "Speed Demon",
+		ef: ["You deal more damage the faster you're moving."],
+		type:"Passive Body Perk",
+		mV:1,
+		aV:0,
+		rL:16
+	},//0
+	{
+		name: "Momentum Shift",
+		ef: ["Defeating an enemy increases movement speed by 10% for 10 sec.",
+		"Defeating an enemy increases movement speed by 15% for 10 sec.",
+		"Defeating an enemy increases movement speed by 20% for 10 sec."],
+		type:"Passive Body Perk",
+		mV:3,
+		aV:0,
+		rL:11
+	},
+	{
+		name: "Pump it, Louder!",
+		ef: ["Reduces recoil of Shotguns and Light Machine Guns by 10%.",
+		"Reduces recoil of Shotguns and Light Machine Guns by 20%."],
+		type:"Passive Body Perk",
+		mV:2,
+		aV:0,
+		rL:5
+	},
+	{
+		name: "Dead Center",
+		ef: ["Increases damage to torsos by 10%.",
+		"Increases damage to torsos by 20%.",],
+		type:"Passive Body Perk",
+		mV:2,
+		aV:0,
+		rL:7
+	},
+	{
+		name: "Heavy Lead",
+		ef: ["Shotguns and Light Machine Guns knocks back enemies with more force."],
+		type:"Passive Body Perk",
+		mV:1,
+		aV:0,
+		rL:12
+	},//4
+	{
+		name: "Poppin' Off",
+		ef: ["Shotguns have a 25% higher chance of dismembering enemies.",
+		"Shotguns have a 50% higher chance of dismembering enemies."],
+		type:"Passive Body Perk",
+		mV:2,
+		aV:0,
+		rL:18
+	},
+	{
+		name: "Manic",
+		ef: ["When entering combat, your movement speed increases by 20% for 10 sec."],
+		type:"Passive Body Perk",
+		mV:1,
+		aV:0,
+		rL:14
+	},
+	{
+		name: "Mongoose",
+		ef: ["Increases Evasion by 25% while reloading."],
+		mV:1,
+		aV:0,
+		rL:9
+	},
+	{
+		name: "Hail of Bullets",
+		ef: ["Shotguns and Light Machine Guns deal 3% more damage.",
+		"Shotguns and Light Machine Guns deal 6% more damage.",
+		"Shotguns and Light Machine Guns deal 9% more damage."],
+		type:"Passive Body Perk",
+		mV:3,
+		aV:0,
+		rL:1
+	},
+	{
+		name: "BloodRush",
+		ef: ["Increases movement speed in combat by 5% while carrying a Shotgun or Light Machine Gun.",
+		"Increases movement speed in combat by 10% while carrying a Shotgun or Light Machine Gun."],
+		type:"Passive Body Perk",
+		mV:2,
+		aV:0,
+		rL:7
+	},
+	{
+		name: "Skeet Shooter",
+		ef: ["Deal 15% more damage to moving targets."],
+		type:"Passive Body Perk",
+		mV:1,
+		aV:0,
+		rL:12
+	},
+	{
+		name: "Hit the Deck",
+		ef: ["Increases damage to staggered and knock-down enemies by 10%.",
+		"Increases damage to staggered and knock-down enemies by 20%."],
+		type:"Passive Body Perk",
+		mV:1,
+		aV:0,
+		rL:18
+	},
+	{
+		name: "Burn Baby Burn",
+		ef: ["Doubles the duration of burn."],
+		type:"Passive Body Perk",
+		mV:1,
+		aV:0,
+		rL:16
+	},
+	{
 		name: "Epimorphosis",
-		ef: ["Health regenerates up to 70% of max Health outside of combat.",
-		"Health regenerates up to 80% of max Health outside of combat.",
-		"Health regenerates up to 90% of max Health outside of combat."],
+		ef: ["Increases Crit Damage with Shotguns and Light Machine Guns by 15%",
+		"Increases Crit Damage with Shotguns and Light Machine Guns by 30%",
+		"Increases Crit Damage with Shotguns and Light Machine Guns by 45%"],
 		type:"Passive Body Perk",
 		mV:3,
 		aV:0,
@@ -285,7 +322,7 @@ var annihilation = [
 		"Health regenerates up to 80% of max Health outside of combat.",
 		"Health regenerates up to 90% of max Health outside of combat."],
 		type:"Passive Body Perk",
-		mV:3,
+		mV:2,
 		aV:0,
 		rL:7
 	},
@@ -295,7 +332,7 @@ var annihilation = [
 		"Health regenerates up to 80% of max Health outside of combat.",
 		"Health regenerates up to 90% of max Health outside of combat."],
 		type:"Passive Body Perk",
-		mV:3,
+		mV:1,
 		aV:0,
 		rL:7
 	},
@@ -305,97 +342,23 @@ var annihilation = [
 		"Health regenerates up to 80% of max Health outside of combat.",
 		"Health regenerates up to 90% of max Health outside of combat."],
 		type:"Passive Body Perk",
-		mV:3,
+		mV:1,
 		aV:0,
 		rL:7
 	},
 	{
 		name: "Epimorphosis",
-		ef: ["Health regenerates up to 70% of max Health outside of combat.",
-		"Health regenerates up to 80% of max Health outside of combat.",
-		"Health regenerates up to 90% of max Health outside of combat."],
+		ef: [""],
 		type:"Passive Body Perk",
-		mV:3,
+		mV:1,
 		aV:0,
 		rL:7
 	},
 	{
-		name: "Epimorphosis",
-		ef: ["Health regenerates up to 70% of max Health outside of combat.",
-		"Health regenerates up to 80% of max Health outside of combat.",
-		"Health regenerates up to 90% of max Health outside of combat."],
+		name: "final",
+		ef: [""],
 		type:"Passive Body Perk",
-		mV:3,
-		aV:0,
-		rL:7
-	},
-	{
-		name: "Epimorphosis",
-		ef: ["Health regenerates up to 70% of max Health outside of combat.",
-		"Health regenerates up to 80% of max Health outside of combat.",
-		"Health regenerates up to 90% of max Health outside of combat."],
-		type:"Passive Body Perk",
-		mV:3,
-		aV:0,
-		rL:7
-	},
-	{
-		name: "Epimorphosis",
-		ef: ["Health regenerates up to 70% of max Health outside of combat.",
-		"Health regenerates up to 80% of max Health outside of combat.",
-		"Health regenerates up to 90% of max Health outside of combat."],
-		type:"Passive Body Perk",
-		mV:3,
-		aV:0,
-		rL:7
-	},
-	{
-		name: "Epimorphosis",
-		ef: ["Health regenerates up to 70% of max Health outside of combat.",
-		"Health regenerates up to 80% of max Health outside of combat.",
-		"Health regenerates up to 90% of max Health outside of combat."],
-		type:"Passive Body Perk",
-		mV:3,
-		aV:0,
-		rL:7
-	},
-	{
-		name: "Epimorphosis",
-		ef: ["Health regenerates up to 70% of max Health outside of combat.",
-		"Health regenerates up to 80% of max Health outside of combat.",
-		"Health regenerates up to 90% of max Health outside of combat."],
-		type:"Passive Body Perk",
-		mV:3,
-		aV:0,
-		rL:7
-	},
-	{
-		name: "Epimorphosis",
-		ef: ["Health regenerates up to 70% of max Health outside of combat.",
-		"Health regenerates up to 80% of max Health outside of combat.",
-		"Health regenerates up to 90% of max Health outside of combat."],
-		type:"Passive Body Perk",
-		mV:3,
-		aV:0,
-		rL:7
-	},
-	{
-		name: "Epimorphosis",
-		ef: ["Health regenerates up to 70% of max Health outside of combat.",
-		"Health regenerates up to 80% of max Health outside of combat.",
-		"Health regenerates up to 90% of max Health outside of combat."],
-		type:"Passive Body Perk",
-		mV:3,
-		aV:0,
-		rL:7
-	},
-	{
-		name: "Epimorphosis",
-		ef: ["Health regenerates up to 70% of max Health outside of combat.",
-		"Health regenerates up to 80% of max Health outside of combat.",
-		"Health regenerates up to 90% of max Health outside of combat."],
-		type:"Passive Body Perk",
-		mV:3,
+		mV:1,
 		aV:0,
 		rL:7
 	}
--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 var perks = all_perks[0];
+var current_skill = 0;
+var bool_skills = [0,0,0,0,0,0,0,0,0,0,0,0];
+var skill_size = [0,0,0,0,0,0,0,0,0,0,0,0];
 
+
+function calcSkillSize(){
+	var aux = 0;
+	for (var i = 0; i < all_perks.length; i++) {
+		aux=0;
+		for (var j = 0; j < all_perks[i].length; j++) {
+			if(all_perks[i][j].mV < 2){
+				aux += 1;
+			}
+			else{
+				aux += 2;
+			}
+		}
+		skill_size[i] = ~~((aux+5)/6);
+		console.log(skill_size[i]);
+	}	
+}
+
+
+
+//transform current perks to binary string
 function to_binary(){
 	var nDig = 0
 	for (var i = 0; i < perks.length; i++) {
@@ -10,17 +34,14 @@ function to_binary(){
 			nDig = nDig + 2;
 		}
 	}
-	//console.log(nDig);
 	var binHash = ""
 	for (var i = 0; i < perks.length; i++) {
 		if(perks[i].mV > 1 && perks[i].aV < 2){
 			binHash += "0";
 		}
 		binHash += (perks[i].aV >>> 0).toString(2);
-		//binHash += ",";
 	}
 	return binHash;
-	//console.log(binHash);
 }
 
 function fromBinary(hsh){
@@ -37,7 +58,6 @@ function fromBinary(hsh){
 		else{
 			perks[i].aV = parseInt(hsh.slice(index, index+size),2);
 		}
-		//console.log(perks[i].aV);
 		index = index + size;
 	}
 }
@@ -71,7 +91,6 @@ function base64_tobinary(hsh){
 		}
 		else {
 			aux = hsh[i].charCodeAt(0);
-			//console.log(hsh[i],aux);
 			if(aux >= 65 && aux <= 90){
 				binary += (aux-65).toString(2);
 			}
@@ -89,23 +108,9 @@ function base64_tobinary(hsh){
 			final += "0";
 		}
 		final += binary;
-		//console.log(final.length);
 	}
 	return final;
-
 }
-
-var getParams = function (url) {
-	var params = {};
-	var parser = document.createElement('a');
-	parser.href = url;
-	var query = parser.search.substring(1);
-	return query;
-};
-
-var getHash = function (url) {
-	return url.split("#")[1];
-};
 
 function getURLHash(){
 	return window.location.hash.slice(1,window.location.hash.length);
@@ -113,11 +118,9 @@ function getURLHash(){
 
 
 function setClickAction(){
-	//console.log("cheguei");
 	var bt;
 	bt = document.querySelectorAll(".myDiv1");
 	for (var i = 0; i < perks.length; i++) {
-		//console.log(bt[i].title);
 		if(perks[i].aV == 0){
 			bt[i].style.background = "#ffffff";
 		}
@@ -155,9 +158,7 @@ function showDescription(){
 
 
 function clicounacoisa(){
-	//console.log(this.id);
 	var value = parseInt(this.id);
-	//console.log(value);
 	if(perks[value].aV<perks[value].mV){
 		perks[value].aV++;
 	}
@@ -179,21 +180,80 @@ function clicounacoisa(){
 	status.innerHTML = String(perks[value].aV) + '/'+ String(perks[value].mV);
 	var status = document.getElementById("description_cl_ml");
 	status.innerHTML = "Current level: " + String(perks[value].aV) + "/" + String(perks[value].mV);
-	window.location.hash = binary_tobase64(to_binary());
-	//console.log(base64_tobinary(getURLHash()))
+	setURLHash();
+	//window.location.hash = binary_tobase64(to_binary());
 }
 
-function loadAttributeMenu(){
+function setURLHash(){
+	var hsh = "";
+	for (var i = 0; i < all_perks.length; i++) {
+		for (var j = 0; j < all_perks[i].length; j++) {
+			if(all_perks[i][j].aV != 0){
+				bool_skills[i] = 1;
+				break;
+			}
+		}
+	}
+	var hsh1 = "";
+	for (var i = 0; i < bool_skills.length; i++) {
+		console.log(bool_skills[i]);
+		hsh1 += String(bool_skills[i]);
+	}
+	hsh += binary_tobase64(hsh1);
+	console.log(binary_tobase64(hsh1));
+	console.log(hsh1);
+	
+	for (var i = 0; i < bool_skills.length; i++) {
+		if(bool_skills[i] == 1){
+			perks = all_perks[i];
+			hsh += binary_tobase64(to_binary());
+		}
+	}
 
+	window.location.hash = hsh;
 }
 
-function loadWindow(){
+function processURLHash(){
+	var hsh = getURLHash();
+	console.log(hsh);
+	var bool_aux = "";
+	var aux2;
+	var aux3 = "";
+	var auxperks = perks;
+	if(hsh.length < 3){
+		console.log("null url");
+	}
+	else{
+		bool_aux = base64_tobinary(hsh.slice(0, 2));
+		console.log(bool_aux);
+		aux2 = 2;
+		for (var i = 0; i < bool_aux.length; i++) {
+			bool_skills[i] = parseInt(bool_aux[i]);
+			if(bool_skills[i] == 1){
+				aux3 = hsh.slice(aux2,Math.min(aux2+skill_size[i],hsh.length));
+				console.log(aux3);
+				aux2 += skill_size[i]
+				perks = all_perks[i];
+				fromBinary(base64_tobinary(aux3));
+				console.log(base64_tobinary(aux3));
+			}
+		}
+	}
+	perks = auxperks;
+}
+
+
+function loadWindowSkill(){
+	var id = this.getAttribute('data-index');
+	current_skill = id;
+	perks = all_perks[id];
 	var x = document.getElementById("mainWindow");
-	//console.log(x);
+	
+	processURLHash();
+	//hash = getURLHash();
+	//fromBinary(base64_tobinary(hash));
+	
 
-	hash = getURLHash();
-	//if(hash.length <)
-	fromBinary(base64_tobinary(hash));
 	x.innerHTML = "";
 	console.log("teste");
     for (var i = 0; i < perks.length; i++ ){
@@ -208,7 +268,80 @@ function loadWindow(){
 			//position = 58;
 		}
 
+		//'style = "margin-right: 2vw; margin-left: 2vw;"'		
 
+		//'style = "left:' + String(position) + '%;
+
+    	x.innerHTML += '<div title="' + String(perks[i].effect) +
+    	'" class = "myDiv1" id = "' + String(i) +'" '+
+    	margem +
+    	 '> ' + String(perks[i].name) +
+    	 
+    	 '<p class="skill_level" id = "'+ 'level' + String(i) +'">' + String(perks[i].aV) + '/'+ String(perks[i].mV) + '</p>' + ' </div>\n';
+    	if(((i+1)%7) == 0){
+    		x.innerHTML += '<div class="break"></div>\n';
+    	}
+    }
+
+    setClickAction();
+}
+
+function loadSkillsSubMenu(){
+	var bt;
+	var id = this.getAttribute('data-index');
+	console.log(id);
+	var ls_index = [[0,1,2],[3,4,5],[6,7],[8,9],[10,11]];
+	var list_skills = ["Athletics","Annihilation","Street Brawler",
+		"Assault","Handguns","Blades",
+		"Crafting","Engineering",
+		"Stealth","Cool Blood",
+		"Breach Protocol","Quickhacking"];
+
+	var x =	document.getElementById("skill_selector_div");
+	x.innerHTML = "";
+	for (var i = 0; i < ls_index[id].length; i++) {
+		x.innerHTML += '<div class="Attribute_button" '+
+		'data-index = "'+ls_index[id][i]+'" '+
+		'id="skill_button'+ls_index[id][i]+'"'
+		+'>'+list_skills[ls_index[id][i]]+'</div>\n';
+	
+	}
+	for (var i = 0; i < ls_index[id].length; i++) {
+		bt = document.getElementById("skill_button"+ls_index[id][i]);
+		bt.addEventListener('click',loadWindowSkill);
+	}
+	// for (var i = 0; i < lista_buttons.length; i++) {
+	// 	bt = document.getElemenById(lista_buttons[i]);
+	// 	bt.addEventListener('click',clickOnAttributeButton);
+	// }
+}
+
+function loadAttributeMenu(){
+	var bt;
+	var list_buttons = ["ab_body","ab_reflex","ab_technical","ab_cool","ab_intel"];
+	for (var i = 0; i < list_buttons.length; i++) {
+		bt = document.getElementById(list_buttons[i]);
+		bt.addEventListener('click',loadSkillsSubMenu);
+	}
+}
+
+function loadWindow(){
+	var x = document.getElementById("mainWindow");
+	//hash = getURLHash();
+	//fromBinary(base64_tobinary(hash));
+	x.innerHTML = "";
+	console.log("teste");
+    for (var i = 0; i < perks.length; i++ ){
+		//console.log("teste")
+		var position = 14+6*(i%7);
+		var margem = '';
+		if((~~(i/7) == 1)){
+			margem = 'style = "margin-right: 1.5vw; margin-left: 1.5vw;" ';
+			//position = 8+8*(i%7);
+		}
+		else if((~~(i/7) == 3)){
+			//position = 58;
+		}
 
 		//'style = "margin-right: 2vw; margin-left: 2vw;"'		
 
@@ -229,18 +362,15 @@ function loadWindow(){
 
 }
 
-
-//window.addEventListener("load",divclick,false);
-
-
-console.log(window.location.href)
-
+//var perks = all_perks[0];
 
 if(document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', loadWindow);
+    document.addEventListener('DOMContentLoaded', loadAttributeMenu);
 } else {
-    loadWindow();
+    loadAttributeMenu();
 }
+
+calcSkillSize();
 
 console.log(document.getElementById("mainWindow"));
 
